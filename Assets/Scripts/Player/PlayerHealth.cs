9cffff4654e72b113171e1ed480fb53ad7190402
Shaun <sme170330@utdallas.edu>
2020-11-09 18:48:38 -0600

    Controller & UI Setup

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerHealth : MonoBehaviour
+{
+    public static int health = 100;
+
+    public AudioSource _damaged;
+    public AudioSource _dead;
+    public bool isActive;
+
+    UIManager uiManager;
+    public GameObject deathMenuUI;
+
+    private void Awake()
+    {
+        uiManager = FindObjectOfType<UIManager>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            DamagePlayer(12);
+        }
+        if (health == 0)
+        {
+
+            Kill();
+
+        }
+    }
+    public void DamagePlayer(int _damageAmount)
+    {
+
+        //subtract health6
+        //_damaged.Play();
+        health -= _damageAmount;
+        UnityEngine.Debug.Log("Damaged player");
+        if (health < 0)
+        {
+            health = 0;
+            _dead.Play();
+        }
+        //update slider
+        uiManager.UpdateHealthSlider();
+    }
+   
+    public void Kill()
+    {
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isActive = true;
+
+        Pause();
+        UnityEngine.Debug.Log("player set inactive, ur dead");
+
+    }
+    public void Pause()
+    {
+        if (isActive == true)
+        {
+            deathMenuUI.SetActive(true);
+            Cursor.visible = true;
+            Time.timeScale = 0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Ui/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public Slider healthSlider;
+    public Slider staminaSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void UpdateHealthSlider()
+    {
+        //set slider value = to health
+        healthSlider.value = PlayerHealth.health;
+    }
+
+    public void UpdateStaminaSlider()
+    {
+        //set stamina slider
+        staminaSlider.value = FPSController.stamina;
+    }
+}
